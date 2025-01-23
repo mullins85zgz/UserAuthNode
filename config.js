915da7fd899f16e5312c214985607bc172ef9ea1
Jse Angel Salinas <jangelsalinas@gmail.com>
2025-01-23 19:27:06 +0100

    Implement JWT authentication and add protected routes with session management

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 export const {
   PORT = 3000,
-  SALT = 10
+  SALT = 10,
+  SECRET_JWT_KEY = 'MiClaveSecreta'
 } = process.env
--- index.js
+++ index.js
@@ -1,20 +1,51 @@
 import express from 'express'
-import { PORT } from './config.js'
+import jwt from 'jsonwebtoken'
+import cookieParser from 'cookie-parser'
+
+import { PORT, SECRET_JWT_KEY } from './config.js'
 import { UserRepository } from './user-repository.js'
 
 const app = express()
 
+app.set('view engine', 'ejs')
+
 app.use(express.json())
+app.use(cookieParser())
+
+app.use((req, res, next) => {
+  const token = req.cookies.access_token
+  let datos = null
+
+  req.session = { user: null }
+  try {
+    datos = jwt.verify(token, SECRET_JWT_KEY)
+    req.session.user = datos
+  } catch (error) {}
+  next()
+})
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  const user = req.session.user
+  res.render('example', { name: user?.username })
 })
 
 app.post('/login', async (req, res) => {
   const { username, password } = req.body
   try {
     const user = await UserRepository.login({ username, password })
-    res.json({ user })
+    const token = jwt.sign({ id: user._id, username: user.username }, SECRET_JWT_KEY,
+      { expiresIn: '1h' }
+    )
+    const refreshToken = jwt.sign({ id: user._id, username: user.username }, SECRET_JWT_KEY,
+      { expiresIn: '7d' }
+    )
+    res.cookie('access_token', token, {
+      httpOnly: true, // la cookie no puede ser leída por el cliente
+      secure: process.env.NODE_ENV === 'production', // solo se envía en conexiones HTTPS
+      sameSite: 'strict', // la cookie no se envía en peticiones de otros dominios
+      maxAge: 1000 * 60 * 60 // tiempo de vida de la cookie en milisegundos. 1 hora
+    }).send({ user, token })
+    // res.json({ user })
   } catch (error) {
     res.status(401).json({ error: error.message })
   }
@@ -31,9 +62,22 @@ app.post('/register', async (req, res) => {
     res.status(400).json({ error: error.message })
   }
 })
-app.post('/logout', (req, res) => {})
+app.post('/logout', (req, res) => {
+  res.clearCookie('access_token').send('Logged out')
+})
 
-app.get('/protected', (req, res) => {})
+app.get('/protected', (req, res) => {
+  const user = req.session.user
+  if (!user) {
+    return res.status(401).send('No tienes permisos')
+  }
+
+  try {
+    res.render('protected', user)
+  } catch (error) {
+    return res.status(401).send('No tienes permisos')
+  }
+})
 
 app.listen(PORT, () => {
   console.log(`Server is running on URL http://localhost:${PORT}`)
